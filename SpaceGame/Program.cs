746e7be12f8df830a3bce4b32c7e48f60960cbd6
Matthew <Allenm40@my.erau.edu>
2019-07-23 14:25:50 -0400

    I fixed the rows and columns place in the fieldDisplay array. I added two methods to Main (DisplaySetUp and DisplayOutput).  DisplaySetUp sets fieldDisplay up to have borders and a top. DisplayOutput prints the array to the screen.

--- SpaceGame/Program.cs
+++ SpaceGame/Program.cs
@@ -1,25 +1,61 @@
 ﻿using System;
+using System.Drawing;
 
 namespace SpaceGame
 {
     class Program
     {
-        static string[,] fieldDisplay = new string[18, 24];
+        static string[,] fieldDisplay = new string[24, 18];//[rows,columns]
         static void Main(string[] args)
         {
 
 
-            Display();
+            DisplaySetUp();
+            DisplayOutput();
+
+            
 
         }
 
-        public static void Display()
+        public static void DisplaySetUp()
         {
-            for (int i = 0; i < 24; i++)
+            for (int i = 1; i < 17; i++)
             {
-                fieldDisplay[0, i] = "|";
-                fieldDisplay[18, i] = "|";
+                fieldDisplay[0, i ] = "^";
+                
+
+            }
+            for (int i = 1; i < 24; i++)
+            {
+                for (int j = 1; j < 18; j++)
+                {
+                    fieldDisplay[i, j] = " ";
+                }
+            }
+
+
+            for (int j = 0; j < 24; j++)
+            {
+                fieldDisplay[j, 0] = "|";
+              
+                fieldDisplay[j, 17] = "|";
             }
         }
+
+        public static void DisplayOutput()
+        {
+            int rowLeng = fieldDisplay.GetLength(1);
+            int colLeng = fieldDisplay.GetLength(0);
+            for (int j = 0; j < colLeng; j++)
+            {
+                for ( int i = 0; i < rowLeng; i++)
+                {
+                    Console.Write(string.Format("{0} ", fieldDisplay[j, i]));
+                }
+                Console.WriteLine();
+            }
+        }
+
+      
     }
 }
