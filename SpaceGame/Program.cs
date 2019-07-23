5ec63cdb5d48377d6b39583fcf75a284223cf280
Matthew <Allenm40@my.erau.edu>
2019-07-23 12:33:12 -0400

    I made the fieldDisplay array static and defined outside of the main method. Created new method for the display.

--- SpaceGame/Program.cs
+++ SpaceGame/Program.cs
@@ -4,10 +4,22 @@ namespace SpaceGame
 {
     class Program
     {
+        static string[,] fieldDisplay = new string[18, 24];
         static void Main(string[] args)
         {
-            Array[,] fieldDisplay = new Array[18,2];
 
+
+            Display();
+
+        }
+
+        public static void Display()
+        {
+            for (int i = 0; i < 24; i++)
+            {
+                fieldDisplay[0, i] = "|";
+                fieldDisplay[18, i] = "|";
+            }
         }
     }
 }
